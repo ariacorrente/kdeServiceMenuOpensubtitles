eb20d46657b752ab407ad47b3104657c1d39829d
Nicola Felice <dev@dominiofelice.com>
2015-08-14 11:28:52 +0200

    Change destination folder
    Add note about how to publish on kde-apps.org

--- generatePackage.sh
+++ generatePackage.sh
@@ -5,7 +5,12 @@
 # git repository without the .git files and folders.
 # The purpose of this action is to generate the package to publish.
 #
+# WARNING!
+# In kde-apps.org I can't upload a file because the website throws an error. I 
+# solved the problem tagging a release in GitHub and linking to the archive
+# generated automatically.
+#
 
 archiveName=kdeServiceMenuOpenSubtitles_`date --rfc-3339=date`
 
-git archive --format=tar.gz --prefix kdeServiceMenuOpenSubtitles/ -o $archiveName.tar.gz HEAD
+git archive --format=tar.gz --prefix kdeServiceMenuOpenSubtitles/ -o release/$archiveName.tar.gz HEAD
