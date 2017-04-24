d75870ac10db0a62a5b69ff7a736eb7f04c49b6c
Nicola Felice <dev@dominiofelice.com>
2017-04-24 23:03:57 +0200

    Update comments on deploy script
    The generated package now works on the new KDE Store.

--- generatePackage.sh
+++ generatePackage.sh
@@ -5,10 +5,10 @@
 # git repository without the .git files and folders.
 # The purpose of this action is to generate the package to publish.
 #
-# WARNING!
 # In kde-apps.org I can't upload a file because the website throws an error. I 
 # solved the problem tagging a release in GitHub and linking to the archive
 # generated automatically.
+# In the new store.kde.org instead the upload works fine.
 #
 
 archiveName=kdeServiceMenuOpenSubtitles_`date --rfc-3339=date`
